fb614638de9dac354f935dac8276a5932d28d36f
Guilherme Ferreira <guigododoti@gmail.com>
2025-07-16 13:42:03 -0300

    .
    .

---
+++ prisma/migrations/20250716164039_add_category_paymentmethod_relations/migration.sql
@@ -0,0 +1,5 @@
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "Tenant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaymentMethod" ADD CONSTRAINT "PaymentMethod_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "Tenant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
